5238761799dd30f57922888ca81b81d9d8c40114
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-09 16:05:43 +0200

    esercizio 3

---
+++ src/entities/esercizio3/Main.java
@@ -0,0 +1,24 @@
+package entities.esercizio3;
+
+public class Main {
+    static void main(String[] args) {
+
+        Rubrica nuovaRubrica = new Rubrica();
+
+        // Inserisco i contatti
+        nuovaRubrica.inserisciContatto("Antonio", 329876545);
+        nuovaRubrica.inserisciContatto("Roberto", 345567898);
+        nuovaRubrica.inserisciContatto("Alessia", 388756098);
+
+
+        nuovaRubrica.cercaPersona(329876545);// si trova in rubrica
+        nuovaRubrica.cercaPersona(329876542);// non si trova in rubrica
+
+        nuovaRubrica.cercaNumero("Antonio");
+
+        nuovaRubrica.stampaRubrica();
+
+        nuovaRubrica.cancellaContatto("Roberto");
+        nuovaRubrica.stampaRubrica();
+    }
+}
---
+++ src/entities/esercizio3/Rubrica.java
@@ -0,0 +1,65 @@
+package entities.esercizio3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rubrica {
+
+    // interfaccia
+    private HashMap<String, Integer> contatti;
+
+    //costruttore
+    public Rubrica() {
+        this.contatti = new HashMap<>();
+    }
+
+    // METODI
+
+    // 1 Inserimento coppia <Nome, telefono>
+    public void inserisciContatto(String nome, int telefono) {
+        contatti.put(nome, Integer.valueOf(telefono));
+        System.out.println("Contatto aggiunto: " + nome + " " + "Numero telefono: " + telefono);
+    }
+
+    // 2 Cancellazione coppia <Nome, telefono>
+    public void cancellaContatto(String nome) {
+        if (contatti.containsKey(nome)) {
+            contatti.remove(nome);
+            System.out.println("Contatto cancellato: " + nome);
+        } else {
+            System.out.println("Impossibile cancellare: " + nome + " non è in rubrica.");
+        }
+    }
+
+    // 3 Ricerca persona tramite numero
+    public void cercaPersona(int telefono) {
+        for (Map.Entry<String, Integer> entry : contatti.entrySet()) {
+            if (entry.getValue().equals(telefono)) {
+                System.out.println("Contatto trovato, il nome della persona è: " + entry.getKey());
+                return;
+            }
+        }
+        System.out.println("Impossibbile trovare la persona tramite: " + telefono + " perchè non è in rubrica");
+    }
+
+    // 4 Ricerca numero tramite nome
+    public void cercaNumero(String nome) {
+        if (contatti.containsKey(nome)) {
+            System.out.println("Numero della persona trovato: " + contatti.get(nome));
+        } else {
+            System.out.println("Impossibile trovare il numero della persona: " + nome + " percjè la persona non è in rubrica");
+        }
+    }
+
+    // 5 Stampa tutta la rubrica
+    public void stampaRubrica() {
+        System.out.println("***** Elenco Rubrica *****");
+        if (contatti.isEmpty()) {
+            System.out.println("L'elenco della rubrica è vuoto.");
+        } else {
+            for (Map.Entry<String, Integer> entry : contatti.entrySet()) {
+                System.out.println("Nome: " + entry.getKey() + " | Telefono: " + entry.getValue());
+            }
+        }
+    }
+}
